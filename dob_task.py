c7f96b7446e149e391f6fde70a8233c269e27791
Mikemupararano <kudath@yahoo.co.uk>
2024-04-05 18:18:59 +0100

    Added comments to code.

--- dob_task.py
+++ dob_task.py
@@ -4,24 +4,24 @@ def read(filename):
 
     Args:
         filename (str): The name of the text file.
-
     """
     try:
         with open(filename, 'r') as file:
-            Names = []  # This is a list that will store all the names read from the DOB text file
-            Birthdates = []  # This is a list that will store all the birthdates read from the DOB text file
-            for line in file:
-                Line = line.strip().split()  # strip any white spaces and split the line into pieces
-                # each line is split into 5 pieces - the first two pieces will be the name and surname while the 3rd,4th and 5th pieces will be the Birthdate
-                Names.append(Line[0] + " " + Line[1])  # Add the first 2 pieces into the Names list.
-                Birthdates.append(' '.join(Line[2:]))  # Add the last 3 pieces into the Birthdates list.
+            Names = []  # List to store names
+            Birthdates = []  # List to store birthdates
 
-            # Print Names
+            # Iterate over each line in the file
+            for line in file:
+                Line = line.strip().split()  # Split the line into words
+                Names.append(Line[0] + " " + Line[1])  # Add the name to the Names list
+                Birthdates.append(' '.join(Line[2:]))  # Add the birthdate to the Birthdates list
+
+            # Print the Names section
             print("Name")
             for name in Names:  
                 print(name)
 
-            # Print Birthdate
+            # Print the Birthdate section
             print("\nBirthdate")
             for birthdate in Birthdates:
                 print(birthdate)
